62b352cf026f25827075bc70507c96cff7e8d0a6
David Kanekanian <dkanekanian1@gmail.com>
2020-11-11 20:51:17 +0000

    Add sprite with animations

--- src/rendering/moonRenderer.js
+++ src/rendering/moonRenderer.js
@@ -1,11 +1,13 @@
 import { Renderer } from 'lance-gg';
-import { Actor, Color, Engine as ExEngine, Loader, Scene, SpriteSheet } from 'excalibur';
+import { Actor, Color, Engine as ExEngine, Loader, LockCameraToActorStrategy, Scene, SpriteSheet } from 'excalibur';
 import resources from './resources';
+import Player from '../core/player';
 
 export default class MoonRenderer extends Renderer {
     constructor(gameEngine, clientEngine) {
         super(gameEngine, clientEngine);
     }
+
     init() {
         // Create the excalibur engine.
         this.initExcalibur();
@@ -24,19 +26,27 @@ export default class MoonRenderer extends Renderer {
     }
 
     makeTestExcaliburScene() {
-        const a = new Actor();
+        const a = this.a = new Actor(0, 0);
         a.onInitialize = function (engine) {
-            const s = new SpriteSheet(resources.character1, 4, 6, 16, 16);
-            this.addDrawing('idle', s.getAnimationBetween(engine, 0, 6, 125));
+            const s = new SpriteSheet(resources.character1, 6, 5, 16, 16);
+            this.addDrawing('walk_r', s.getAnimationBetween(engine, 0, 6, 125));
+            this.addDrawing('walk_l', s.getAnimationBetween(engine, 6, 12, 125));
+            this.addDrawing('idle_r', s.getAnimationBetween(engine, 12, 18, 125));
+            this.addDrawing('idle_l', s.getAnimationBetween(engine, 18, 24, 125));
+            this.addDrawing('attack_r', s.getSprite(24));
+            this.addDrawing('attack_l', s.getSprite(25));
+            this.setDrawing('attack_l');
         };
         const testScene = new Scene(this.excaliburEngine);
         testScene.add(a);
+        testScene.camera.zoom(5);
+        // TODO make a custom strategy to lock to two players
+        testScene.camera.addStrategy(new LockCameraToActorStrategy(a));
         this.excaliburEngine.addScene('test', testScene);
+        this.excaliburEngine.goToScene('test');
     }
 
     draw(t, dt) {
         super.draw(t, dt);
-
-        // Draw the players.
     }
 }
