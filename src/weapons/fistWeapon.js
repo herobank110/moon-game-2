63652ae8b21d8d31d5bdc2657652bf856a684213
David <dkanekanian1@gmail.com>
2020-11-13 20:17:43 +0000

    add fist weapon

--- src/weapons/fistWeapon.js
+++ src/weapons/fistWeapon.js
@@ -1,3 +1,33 @@
-import WeaponBase from "../core/baseWeapon";
+import BasePawn from '../core/basePawn';
+import WeaponBase from '../core/baseWeapon';
+import { closestObject, objectsInRange, pawnsInWorld } from '../utils/lanceUtils';
 
-export default class FistWeapon extends WeaponBase { }
+/** Furthest a player can be to be targeted. */
+const attackRadius = 10;
+
+/** Amount to damage when attacking. */
+const damageAmount = 10;
+
+export default class FistWeapon extends WeaponBase {
+    attack() {
+        const wielder = this.getWielder();
+        if (!wielder) {
+            throw new Error('cannot attack when weapon is unwieldy');
+        }
+
+        // @ts-ignore
+        /** @type {BasePawn} */ const target =
+            closestObject(
+                objectsInRange(
+                    pawnsInWorld(this.gameEngine.world),
+                    wielder.position, attackRadius, [wielder]),
+                wielder.position);
+
+        if (target) {
+            // An unlucky pawn was found!
+            target.applyDamage(damageAmount, wielder, null);
+        }
+    }
+
+    syncTo(other) { super.syncTo(other); }
+}
