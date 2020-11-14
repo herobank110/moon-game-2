23792e795872bbb66d17d60982a9dd4a0b0cd0f3
David Kanekanian <dkanekanian1@gmail.com>
2020-11-14 19:22:46 +0000

    remove enemy on death

--- src/pawns/test_enemy.js
+++ src/pawns/test_enemy.js
@@ -1,10 +1,15 @@
 import BasePawn from '../core/basePawn';
+import { hasAuthority } from '../utils';
 
 export class test_Enemy extends BasePawn {
     syncTo(other) { return super.syncTo(other); }
 
     onDied(instigator, reason) {
         super.onDied(instigator, reason);
+
+        if (hasAuthority()) {
+            this.gameEngine.removeObjectFromWorld(this.id);
+        }
         console.log('oh no, the test enemy died!');
     }
 
