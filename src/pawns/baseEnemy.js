c8e2424a62ff0de3a772e120708f75d5c4a8f5bc
David <dkanekanian1@gmail.com>
2020-11-15 16:47:43 +0000

    add ai phase scheduling function implementation

--- src/pawns/baseEnemy.js
+++ src/pawns/baseEnemy.js
@@ -1,5 +1,5 @@
-import { TwoVector } from 'lance-gg';
 import BasePawn from '../core/basePawn';
+import { lerp, randomInRange } from '../utils/mathUtils';
 
 export default class BaseEnemy extends BasePawn {
     static get netScheme() {
@@ -15,23 +15,36 @@ export default class BaseEnemy extends BasePawn {
         /** Object being targeted for attack. */
         this.attackTargetId = -1;
 
-        this.waitAtPointDurationMin = 3;
-        this.waitAtPointDurationMax = 6;
+        this.waitAtPointDurationMin = 3000;
+        this.waitAtPointDurationMax = 6000;
 
-        this.followTargetDurationMin = 1;
-        this.followTargetDurationMax = 1.2;
+        this.followTargetDurationMin = 1000;
+        this.followTargetDurationMax = 1200;
 
+        /** @type {(this: BaseEnemy) => void} */
         this.lastMoveFunc = null;
+        this.aiFuncCounter = null;
     }
 
     /** @returns attack target's object ID, or -1 to disable targeting. */
     pickAttackTarget() { return -1; }
 
     scheduleNextMove() {
-        // TODO
+        let nextMove, delay;
+        if (this.lastMoveFunc == this.waitAtPoint) {
+            nextMove = this.followAttackTarget;
+            delay = randomInRange(this.waitAtPointDurationMin, this.waitAtPointDurationMax);
+        } else {
+            nextMove = this.waitAtPoint;
+            delay = randomInRange(this.followTargetDurationMin, this.followTargetDurationMax);
+        }
+
+        this.lastMoveFunc = nextMove;
+        // @ts-ignore
+        this.aiFuncCounter = setTimeout(nextMove.bind(this), delay);
     }
 
-    followTarget() {
+    followAttackTarget() {
         // TODO
     }
 
@@ -40,4 +53,13 @@ export default class BaseEnemy extends BasePawn {
     }
 
     syncTo(other) { super.syncTo(other); }
+
+    onDied(instigator, reason) {
+        super.onDied(instigator, reason);
+
+        // Cancel any next move.
+        if (this.aiFuncCounter !== null) {
+            clearTimeout(this.aiFuncCounter);
+        }
+    }
 }
