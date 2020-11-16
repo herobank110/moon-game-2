9de3e8a8770b2af26cf25555aeeba0c2698bdbe5
David Kanekanian <dkanekanian1@gmail.com>
2020-11-16 17:54:21 +0000

    smooth out camera track and avoid net

--- src/rendering/cameraFocalPoint.js
+++ src/rendering/cameraFocalPoint.js
@@ -4,6 +4,15 @@ import { check, hasAuthority } from "../utils";
 
 /** [client] always at players average position each frame */
 export default class CameraFocalPoint extends DynamicObject {
+    static get netScheme() { return {}; }
+
+    constructor(gameEngine, options, props) {
+        super(gameEngine, options, props);
+        // Ignore any collisions.
+        this.width = 0;
+        this.height = 0;
+    }
+
     onAddToWorld(gameEngine) {
         if (!hasAuthority()) {
             // Only bother using this on clients.
@@ -16,10 +25,11 @@ export default class CameraFocalPoint extends DynamicObject {
         /** @ts-ignore @type {MoonEngine} */
         const ge = this.gameEngine;
         const players = ge.getPlayers();
-        this.position
-            .copy(players.reduce(
-                (x, y) => x.add(y.position), new TwoVector(0, 0)))
-            .multiplyScalar(1 / players.length);
+        // perform lerp for smoother motion.
+        this.position.lerp(
+            players.reduce((x, y) => x.add(y.position), new TwoVector(0, 0))
+                .multiplyScalar(1 / players.length),
+            0.4);
         console.log(this.position);
     }
 
