b20519a6a7271010852e08d36cffed7e046772ae
David <dkanekanian1@gmail.com>
2020-11-19 17:40:24 +0000

    add teleport players into elevator function

--- src/core/elevator.js
+++ src/core/elevator.js
@@ -1,10 +1,13 @@
+import { check } from "../utils";
 import { makeInvisibleWall } from "../utils/lanceUtils";
 import MoonEngine from "./moonEngine";
 
 const maxHeight = 300;
 
-/** @param {MoonEngine} gameEngine */
+/** [server] start an animated elevator descent.
+ * @param {MoonEngine} gameEngine */
 export function startElevatorSequence(gameEngine) {
+    // Make the elevator collision walls.
     for (const rect of [
         { x: 128,       y: maxHeight,      w: 112, h: 32 },
         { x: 128,       y: maxHeight + 64, w: 112, h: 32 },
@@ -13,9 +16,15 @@ export function startElevatorSequence(gameEngine) {
     ]) {
         gameEngine.addObjectToWorld(makeInvisibleWall(rect));
     }
+
+    // Put players in the elevator.
+    const players = gameEngine.getPlayers();
+    check(players.length == 2, 'must be 2 players for elevator to start');
+    players[0].position.set(128 + 16 * 1, maxHeight + 48);
+    players[1].position.set(128 + 16 * 3, maxHeight + 48);
 }
 
 /** Assumes constant tick rate. Must call startElevatorSequence() first. */
 export function tickElevatorSequence() {
-
+    // TODO: move elevator collision objects down
 }
