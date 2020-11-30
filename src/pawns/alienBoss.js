57ca42465c4105b334c46a1083ae7ddefae5b4d2
David Kanekanian <dkanekanian1@gmail.com>
2020-11-30 00:05:13 +0000

    add slightly different alien boss ai logic

--- src/pawns/alienBoss.js
+++ src/pawns/alienBoss.js
@@ -1,7 +1,15 @@
 import BaseEnemy from "../core/baseEnemy";
 
 export default class AlienBoss extends BaseEnemy {
-    static get initialHealth() { return 100; }
+    static get initialHealth() { return 200; }
+    constructor(gameEngine, options, props) {
+        super(gameEngine, options, props);
+        this.followTargetDurationMin = 1.0;
+        this.followTargetDurationMax = 1.5;
+
+        this.waitAtPointDurationMin = 9;
+        this.waitAtPointDurationMax = 13;
+    }
 
     syncTo(other) { super.syncTo(other); }
 }
