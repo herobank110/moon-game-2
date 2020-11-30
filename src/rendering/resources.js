430a30ebd2d781562e32a72405a053d174d839c1
David Kanekanian <dkanekanian1@gmail.com>
2020-11-30 23:36:41 +0000

    add sound resources to game

--- src/rendering/resources.js
+++ src/rendering/resources.js
@@ -1,4 +1,4 @@
-import { Texture } from 'excalibur';
+import { Sound, Texture } from 'excalibur';
 
 const resources = {
     character1: new Texture('./character_animations.png'),
@@ -9,7 +9,11 @@ const resources = {
     staticProps: new Texture('./static_props.png'),
     fist: new Texture('./weapon_fist.png'),
     elevatorBack: new Texture('./elevator_back.png'),
-    elevatorFront: new Texture('./elevator_front.png')
+    elevatorFront: new Texture('./elevator_front.png'),
+    elevatorMusic: new Sound('./elevator_music.mp3'),
+    menuMusic: new Sound('./menu_music.mp3'),
+    battleMusic: new Sound('./battle_music.mp3'),
+    alienRoar: new Sound('./alien_roar.mp3'),
 };
 
 export default resources;
