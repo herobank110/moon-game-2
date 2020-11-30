0df4c5e020e35c84af06596bc593df2643d535c3
David Kanekanian <dkanekanian1@gmail.com>
2020-11-30 00:14:24 +0000

    alternate target for boss enemy each phase

--- src/pawns/alienBoss.js
+++ src/pawns/alienBoss.js
@@ -1,14 +1,35 @@
 import BaseEnemy from "../core/baseEnemy";
+import MoonEngine from "../core/moonEngine";
+import { hasAuthority } from "../utils";
 
 export default class AlienBoss extends BaseEnemy {
     static get initialHealth() { return 200; }
     constructor(gameEngine, options, props) {
         super(gameEngine, options, props);
-        this.followTargetDurationMin = 1.0;
-        this.followTargetDurationMax = 1.5;
+        this.lastTarget = null;
+        this.followTargetDurationMin = 2000;
+        this.followTargetDurationMax = 3000;
+    }
 
-        this.waitAtPointDurationMin = 9;
-        this.waitAtPointDurationMax = 13;
+    /** [server] Alternate attack target each time. */
+    pickAttackTarget() {
+        super.pickAttackTarget()
+        /** @ts-ignore @type {MoonEngine} */
+        // return ge ? closestObject(ge.getPlayers(), this.position)?.id ?? -1 : -1;
+        // if (hasAuthority()) {
+        /** @ts-ignore @type {MoonEngine} */
+        const ge = this.gameEngine;
+        if (ge) {
+            const players = ge.getPlayers();
+            for (const p of players) {
+                if (p.id != this.lastTarget) {
+                    this.lastTarget = p.id;
+                    return p.id;
+                }
+            }
+        }
+        return -1;
+        // }
     }
 
     syncTo(other) { super.syncTo(other); }
